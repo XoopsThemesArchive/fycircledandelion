fb9ba0744be09c863047f478edcd943ead8c1fbc
mambax7 <mambax7@gmail.com>
2015-05-05 09:03:31 -0400

    Original files

---
+++ fycircledandelion/manifesto.ini.php
@@ -0,0 +1,17 @@
+<?php
+/**
+
+[Manifesto]
+Name="FY Circle Parsley"
+Depends=Legacy_RenderSystem
+Url="http://www.theme4u.net/"
+Version="Update: 2006/01/15"
+
+[Theme]
+Author=Broom
+ScreenShot="screenshot.png"
+Description="FY Circle Series - Parsley"
+W3C=OK
+
+*/
+?>
